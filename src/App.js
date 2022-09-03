28ac96bdeaca612bd71a40d28466320700493094
ayush-mathpal <92756482+AyushMathpal@users.noreply.github.com>
2022-09-03 14:02:10 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DirectPageTestimonials from './components/DirectPageTestimonials/DirectPageTestimonials'
+import TrackAppointment from './components/trackYourAppointment/trackYourAppointment'
+
+
+function App() {
+  return (
+    <>
+    <TrackAppointment/>
+    <DirectPageTestimonials/>
+    </>
+    );
+}
+
+export default App;
